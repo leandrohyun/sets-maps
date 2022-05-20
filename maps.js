c3a9d9db2e534447e8b9130b5abfcc2fcadfd5ad
Leandro Hyun <leandro.souza@icen.ufpa.br>
2022-05-20 13:34:00 -0300

    sets-maps

---
+++ maps.js
@@ -0,0 +1,6 @@
+/*
+Maps será usado para identificar edifícios de uma determinada avenida com base em sua ordem, 
+esses edifícios serão numerados de acordo com sua ordem, onde os números serão usados como 
+“key” para acessar suas informações, como nome, quantidade de moradores e outros como “value”.
+
+*/
---
+++ sets.js
@@ -0,0 +1,9 @@
+/*
+Sets será usado para analisar os serviços (segurança, saúde, educação e transporte) 
+entre os distritos da cidade.
+Desse modo, cada serviço atua por meio de uma construção em algum ponto do distrito para
+realizar os serviços prestados (ex: Delegacia, Bombeiros, Escolas, Bibliotecas e Terminais de transporte).
+Logo, essas edificações serão organizadas em conjuntos em cada distrito.
+Com isso, será possível fazer comparações de serviços em cada distrito da cidade para que eles possam atender
+de maneira mais justa todos os pontos da cidade.
+*/
